1c0070e0482cf4a7c97460d8c118f9b4f306f025
Mohammad Saad Bhaldar <saadbhaldar1212@gmail.com>
2023-12-27 15:12:38 +0530

    @Min, @Max validation annotation example

--- 07-spring-mvc/06-initbinder-form-validation/src/main/java/com/tutorial/formvalidation/model/Customer.java
+++ 07-spring-mvc/06-initbinder-form-validation/src/main/java/com/tutorial/formvalidation/model/Customer.java
@@ -1,17 +1,31 @@
 package com.tutorial.formvalidation.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
-	
-	@NotNull(message="is required")
-	@Size(min=1, message="is required")
+
+	@NotNull(message = "is required")
+	@Size(min = 1, message = "is required")
 	private String firstName;
-	
-	@NotNull(message="is required")
-	@Size(min=1, message="is required")
+
+	@NotNull(message = "is required")
+	@Size(min = 1, message = "is required")
 	private String lastName;
+
+	@Min(value = 0, message = "value should be greater than or equal to 0")
+	@Max(value = 10, message = "value should be less than or equal to 10")
+	private int freePasses;
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
 
 	public String getFirstName() {
 		return firstName;
@@ -32,6 +46,5 @@ public class Customer {
 	public Customer() {
 		super();
 	}
-	
-	
+
 }
