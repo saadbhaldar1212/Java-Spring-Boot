9355f1a9b14963311af49abbd3b3b6322bb41ade
Mohammad Saad Bhaldar <saadbhaldar1212@gmail.com>
2023-12-27 10:26:33 +0530

    imports organzing

--- 07-spring-mvc/04-data-binding/src/main/java/com/tutorial/thymeleafspringboot/controller/ThymeleafRestController.java
+++ 07-spring-mvc/04-data-binding/src/main/java/com/tutorial/thymeleafspringboot/controller/ThymeleafRestController.java
@@ -8,11 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tutorial.thymeleafspringboot.model.Registration;
 
-import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class ThymeleafRestController {
